80322b7bd5401ecfa7d943282cd97913240eb9d2
james <james500nju@gmail.com>
2025-01-20 08:28:44 +0300

    routes, usercrude operation

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { deleteUser, getAllUsers, getSingleUser, postUser, updateUser} from "../controllers/users";
+import { deleteUser, getAllUsers, getSingleUser, Login, postUser, updateUser} from "../controllers/users";
 
 const routes = express.Router();
 
 // Define routes with RESTful conventions
-routes.post("/user", postUser);
+routes.post("/register", postUser);
+routes.post("/login", Login);
 routes.get("/users", getAllUsers);
 routes.get('/:id/user', getSingleUser);
 routes.patch("/:id/user", updateUser);
